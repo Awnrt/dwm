f8dd2dfb2c5a5faa72302c9ea579976a93442076
Awnrt <74102659+Awnrt@users.noreply.github.com>
2022-10-27 16:54:21 +0300

    Update config.h

--- config.h
+++ config.h
@@ -15,8 +15,8 @@ static const unsigned int gappov    = 20;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "undefined-medium:size=12" };
-static const char dmenufont[]       = "undefined-medium:size=12";
+static const char *fonts[]          = { "UbuntuMono:size=12" };
+static const char dmenufont[]       = "UbuntuMono:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bd93f9";
@@ -81,7 +81,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[] = { "chromium", NULL };
+static const char *browsercmd[] = { "firefox", NULL };
 static const char *scrotcmd[] = { "/usr/bin/flameshot", "gui", NULL };
 static const char *slockcmd[] = { "/usr/bin/slock", NULL };
 
