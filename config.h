48b92d6ad530da3b4b2d5b2046c0e94cc6f41c69
Awnrt <roman.levikoff@gmail.com>
2022-11-11 15:32:38 +0300

    upload

--- config.h
+++ config.h
@@ -21,7 +21,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee"; //selected foreground (greens is 888888)
-static const char col_cyan[]        = "#bd93f9";
+static const char col_cyan[]        = "#f59542";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -80,8 +80,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[] = { "firefox", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
+static const char *browsercmd[] = { "/opt/firefox/firefox-bin", NULL };
 static const char *scrotcmd[] = { "/usr/bin/flameshot", "gui", NULL };
 static const char *slockcmd[] = { "/usr/bin/slock", NULL };
 
